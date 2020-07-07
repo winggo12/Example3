00679fecaac2f896c44e9a0519b16b9f9fa5d0fc
Louis Rodman <louis@itwakes.me>
2020-07-07 18:38:15 +0800

    Initial Commit

---
+++ Example3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example3
+//
+//  Created by hkuit155 on 7/7/2020.
+//  Copyright © 2020 Hong Kong Univisual Intelligent Technology Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
